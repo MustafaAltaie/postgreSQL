fa182342f1a1d86e9d23fbe0304e3aea8b16fe99
MustafaAltaie <mustafaphoto111@gmail.com>
2024-08-24 14:40:35 +0200

    cgj

--- src/index.ts
+++ src/index.ts
@@ -23,12 +23,46 @@ pool.connect()
 // Middleware för att hantera JSON-data
 app.use(express.json());
 
-app.get('/film', async (req, res) => {
+app.get('/:table', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM film');
+    const result = await pool.query(`SELECT * FROM ${req.params.table}`);
     res.json(result.rows);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to retrieve films' });
+    res.status(500).json({ error: 'Failed to retrieve data' });
+  }
+});
+
+app.get('/:table/:id', async (req, res) => {
+  try {
+    const film = await pool.query(`SELECT * FROM ${req.params.table} WHERE my_id = $1`, [req.params.id]);
+    res.json(film.rows);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to retrieve the film' });
+  }
+});
+
+app.delete('/:table/:id', async (req, res) => {
+  try {
+    await pool.query(`DELETE FROM ${req.params.table} WHERE my_id = $1`, [req.params.id]);
+    res.status(200).json({ message: 'Data deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to delete the data' });
+  }
+});
+
+app.post('/anywhere', async (req, res) => {
+  const { first_name, last_name, email } = req.body;
+  try {
+    const query = `
+    INSERT INTO anywhere (first_name, last_name, email)
+    VALUES ($1, $2, $3)
+    RETURNING *`;
+
+    const result = await pool.query(query, [first_name, last_name, email]);
+
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error('Error inserting film:', err); // Log the error for debugging
   }
 });
 
